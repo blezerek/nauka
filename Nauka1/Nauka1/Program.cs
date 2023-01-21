0fdd2c2d091dad10ec1ba42944d0778e2109f37c
blezerek <szewczykpioter@gmail.com>
2023-01-21 18:29:58 +0100

    Zadanie dzień 3-ci

--- Nauka1/Nauka1/Program.cs
+++ Nauka1/Nauka1/Program.cs
@@ -1,2 +1,23 @@
-﻿ // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Witaj GitHub!");
+﻿// Deklaracje zmiennych
+    string Name = "Ewa";
+    int Wiek = 33;
+    bool Kobieta = true;
+
+    if (Kobieta == true && Wiek < 30)
+    {
+    Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else if ( Kobieta == true && Wiek == 33 )
+    {
+    Console.WriteLine(Name + ", lat " + Wiek);
+    }
+    else if ( Kobieta == false && Wiek < 18)
+    {
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+    }
+    else
+    {
+    Console.WriteLine("Podano zmienne spoza zakresu");
+    };
+    
+ 
